20ff5c83bfb6f429b776d8446ae578a9ed2c1e1c
matheusviriato <matheus.viriato@hotmail.com>
2020-06-01 22:09:29 -0300

    fixing file names

--- components/generics/button/button.test.js
+++ components/generics/button/button.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Button from './button';
+import Button from '.';
 
 describe('Button tests', () => {
   describe('Render tests', () => {
