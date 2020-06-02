68c401a752d234f3cb50181c6c4d0b8360447991
matheusviriato <matheus.viriato@hotmail.com>
2020-06-01 22:22:08 -0300

    adding reset css

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import '../style/reset.css';
+import { Global } from '@emotion/core';
+import { reset } from '../style/reset';
 
 const StarWarsWiki = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global styles={reset} />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default StarWarsWiki;
